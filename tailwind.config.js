dfa5b5c059d2d4e3e6c1a8a0ccb33c05776fbcf4
nkrokokou <kokounkrow@gmail.com>
2026-07-05 17:10:48 +0000

    feat(ui/ux): configure tailwind styles and keyframes for cosmic theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,11 +72,30 @@ module.exports = {
                         backgroundPosition: "350% 50%, 350% 50%",
                     },
                 },
+                marquee: {
+                    from: { transform: "translateX(0)" },
+                    to: { transform: "translateX(-50%)" }
+                },
+                "marquee-reverse": {
+                    from: { transform: "translateX(-50%)" },
+                    to: { transform: "translateX(0)" }
+                },
+                shimmer: {
+                    "0%, 90%, 100%": {
+                        "background-position": "calc(-100% - var(--shimmer-width, 40px)) 0",
+                    },
+                    "30%, 60%": {
+                        "background-position": "calc(100% + var(--shimmer-width, 40px)) 0",
+                    },
+                },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
                 aurora: "aurora 60s linear infinite",
+                marquee: "marquee var(--duration, 40s) linear infinite",
+                "marquee-reverse": "marquee-reverse var(--duration, 40s) linear infinite",
+                shimmer: "shimmer 8s infinite",
             },
         },
     },
